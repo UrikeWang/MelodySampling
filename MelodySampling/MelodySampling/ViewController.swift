8694d8cf1ca52ab2976adf160738f6528c852568
Marvin Lin <atimis19@gmail.com>
2017-07-31 11:24:09 +0800

    Starting Pods installing

--- MelodySampling/MelodySampling/ViewController.swift
+++ MelodySampling/MelodySampling/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.blue
+        self.view.backgroundColor = UIColor.bluegit
         // Do any additional setup after loading the view, typically from a nib.
     }
 
