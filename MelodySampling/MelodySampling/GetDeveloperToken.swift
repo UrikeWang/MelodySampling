29aabbf92e829d38d6ef282bedcf50cfb8e20e4c
Marvin Lin <atimis19@gmail.com>
2017-07-31 23:07:44 +0800

    5 test songs done

--- MelodySampling/MelodySampling/GetDeveloperToken.swift
+++ MelodySampling/MelodySampling/GetDeveloperToken.swift
@@ -20,9 +20,16 @@ func getDeveloperToken() {
     ref.child("tokens").child("developerToken").child("token").observe(.value, with: { (snapshot) in
 
         //回家後動這裡，把他寫成一個 func
+        
+        guard let devToken = snapshot.value
+            else {
+            print("Your server don't have developer token")
+            return
+        }
+        
         let userDefault = UserDefaults.standard
 
-        userDefault.set(snapshot.value, forKey: "devToken")
+        userDefault.set(devToken, forKey: "devToken")
 
         print("Developer token was saved to UserDefault")
 
