8296388244cb9e8a789c0e647f25ee86f338f609
Marvin Lin <atimis19@gmail.com>
2017-07-31 11:23:04 +0800

    Launching test done

--- MelodySampling/MelodySampling/ViewController.swift
+++ MelodySampling/MelodySampling/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.blue
         // Do any additional setup after loading the view, typically from a nib.
     }
 
