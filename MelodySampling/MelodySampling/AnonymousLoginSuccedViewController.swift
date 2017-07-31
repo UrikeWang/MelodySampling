72bc7a25be5ac9383f3f96f5ce8fad7e4997c31e
Marvin Lin <atimis19@gmail.com>
2017-07-31 16:39:45 +0800

    Anonymouse status judgement done

--- MelodySampling/MelodySampling/AnonymousLoginSuccedViewController.swift
+++ MelodySampling/MelodySampling/AnonymousLoginSuccedViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 class AnonymousLoginSuccedViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+
+    @IBOutlet weak var emailTextField: UITextField!
+
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBAction func signUpTapped(_ sender: UIButton) {
+
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -20,16 +30,18 @@ class AnonymousLoginSuccedViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    //不知道為什麼，當判斷使用者是不是 anonymous 放在 ViewDidLoad 就是不會直接觸發
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if let user = Auth.auth().currentUser {
+
+            if user.isAnonymous {
+                print("使用者用暱名登入")
+                resultLabel.text = "使用者用暱名登入"
+            }
+        }
     }
-    */
-
 }
