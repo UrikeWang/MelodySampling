7bc7b8ee9fc50fcf7ed1b87bba7ebe9e343d2a09
Marvin Lin <atimis19@gmail.com>
2017-07-31 15:20:40 +0800

    Creating user func was done

--- MelodySampling/MelodySampling/SignUpViewController.swift
+++ MelodySampling/MelodySampling/SignUpViewController.swift
@@ -7,20 +7,71 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var uIDLabel: UILabel!
+    @IBOutlet weak var emailField: UITextField!
+
+    @IBOutlet weak var passwordField: UITextField!
+
+    @IBOutlet weak var confirmPasswordField: UITextField!
+
+    @IBAction func signUpTapped(_ sender: UIButton) {
+
+        guard let email = emailField.text, let password = passwordField.text, let confirmPassword = confirmPasswordField.text else {
+            let text = "E-mail 或帳號碼密碼錯誤"
+
+            print(text)
+
+            uIDLabel.text = text
+
+            return
+        }
+
+        if password != confirmPassword {
+
+            let text = "兩次輸入的密碼不同"
+
+            print(text)
+
+            uIDLabel.text = text
+
+            return
+        }
+
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+
+            guard let user = user else {
+                print("註冊發生錯誤")
+
+                if let error = error {
+                    print(error)
+                    self.uIDLabel.text = "\(error)"
+                }
+
+                return
+            }
+
+            let text = "註冊成功"
+            print(text)
+            print(user.uid)
+
+            self.uIDLabel.text = "\(text)! 你的 uid 是 \(user.uid)"
+        }
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = UIColor.gray
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
