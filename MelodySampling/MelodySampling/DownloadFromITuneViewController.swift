a976438970da1196287a84418f8fd3fc83028fd8
Marvin Lin <atimis19@gmail.com>
2017-08-01 14:42:08 +0800

    Created Another ViewController to try file save

---
+++ MelodySampling/MelodySampling/DownloadFromITuneViewController.swift
@@ -0,0 +1,64 @@
+//
+//  DownloadFromITuneViewController.swift
+//  MelodySampling
+//
+//  Created by moon on 2017/8/1.
+//  Copyright © 2017年 Marvin Lin. All rights reserved.
+//
+
+import UIKit
+
+class DownloadFromITuneViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+
+    @IBAction func button1Tapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func button2Tapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func button3Tapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func button4Tapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func button5Tapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func startingDownloadTapped(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
