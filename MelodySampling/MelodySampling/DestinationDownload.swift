9f92b6bba934e9ae075bea192e2b8ebbe4f90a81
Marvin Lin <atimis19@gmail.com>
2017-08-01 20:41:57 +0800

    Finish file download, save delete, check

--- MelodySampling/MelodySampling/DestinationDownload.swift
+++ MelodySampling/MelodySampling/DestinationDownload.swift
@@ -12,26 +12,30 @@ import Alamofire
 class DestinationDownload: UIViewController {
 
     @IBOutlet weak var containFielLabel: UILabel!
-    
+
     @IBOutlet weak var resultLabel: UILabel!
-    
+
     @IBAction func startButtonTapped(_ sender: UIButton) {
         
+        let destinationString = NSTemporaryDirectory() + "song1.m4a"
+//        let destination = URL(fileURLWithPath: destinationString)
         
+        let destination: DownloadRequest.DownloadFileDestination = { _, _ in
+            let documentsURL = NSHomeDirectory() + "/tmp/"
+            let fileURL = URL(fileURLWithPath: documentsURL.appending("song2.m4a"))
+            
+            return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
+        }
         
-    }
-    
-    @IBAction func checkButtonTapped(_ sender: UIButton) {
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        Alamofire.download(testSong1, to: destination).response { response in
+            
+            print(response.response)
+        }
+        
 
-        let tempDirectory = NSTemporaryDirectory()
-        resultLabel.text = "\(tempDirectory)"
-        
-        print(tempDirectory)
+    }
+
+    @IBAction func checkButtonTapped(_ sender: UIButton) {
         
         let fileManager = FileManager()
         
@@ -47,6 +51,51 @@ class DestinationDownload: UIViewController {
             print("Something wrong during loading")
         }
         
+        
+    }
+
+    @IBAction func clearButton(_ sender: UIButton) {
+    
+        let fileManager = FileManager()
+        
+        do {
+            let fileList = try fileManager.contentsOfDirectory(atPath: NSTemporaryDirectory())
+            
+            for file in fileList {
+                do {
+                    try fileManager.removeItem(atPath: NSHomeDirectory() + "/tmp/" + file) } catch {
+                    print("can't delete file")
+                }
+            }
+        } catch {
+            print("can't delete file")
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tempDirectory = NSTemporaryDirectory()
+        resultLabel.text = "\(tempDirectory)"
+
+        print(tempDirectory)
+
+        let fileManager = FileManager()
+
+        do {
+            let fileList = try fileManager.contentsOfDirectory(atPath: NSTemporaryDirectory())
+
+            containFielLabel.text = "\(fileList)"
+
+            for file in fileList {
+                print(file)
+            }
+        } catch {
+            print("Something wrong during loading")
+        }
+
         // Do any additional setup after loading the view.
     }
 
