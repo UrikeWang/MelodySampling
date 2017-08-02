b546d44432b10b96cfed941830a8bc5f6756efba
Marvin Lin <atimis19@gmail.com>
2017-08-02 20:40:54 +0800

    Page Layout done

---
+++ MelodySampling/MelodySampling/RetrieveDataViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RetrieveDataViewController.swift
+//  MelodySampling
+//
+//  Created by moon on 2017/8/2.
+//  Copyright © 2017年 Marvin Lin. All rights reserved.
+//
+
+import UIKit
+
+class RetrieveDataViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
