19e403b34b1a34379882d1dbb3d630b6c3714bbb
Marvin Lin <atimis19@gmail.com>
2017-08-08 14:17:38 +0800

    B4 create playsounds with timer

--- MelodySampling/MelodySampling/TimerViewController.swift
+++ MelodySampling/MelodySampling/TimerViewController.swift
@@ -9,38 +9,38 @@
 import UIKit
 
 class TimerViewController: UIViewController {
-    
+
     @IBOutlet weak var label1: UILabel!
-    
+
     @IBOutlet weak var label2: UILabel!
-    
+
     @IBOutlet weak var label3: UILabel!
-    
+
     var timeStart: Double?
-    
+
     var timeEnd: Double?
-    
+
     var timePassed: Double?
-    
+
     @IBAction func leftButtonTapped(_ sender: UIButton) {
-        
+
         timeStart = Date().timeIntervalSince1970
-        
+
         label1.text = "\(timeStart)"
     }
 
     @IBAction func rightBUttonTapped(_ sender: UIButton) {
-        
+
         timeEnd = Date().timeIntervalSince1970
-        
+
         label2.text = "\(timeEnd)"
-        
+
         timePassed = timeEnd! - timeStart!
-        
+
         label3.text = "\(timePassed)"
-        
+
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -51,5 +51,5 @@ class TimerViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
 }
