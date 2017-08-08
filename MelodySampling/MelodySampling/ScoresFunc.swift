df64bd8a99df147a9f477a05daefe472e07972a0
Marvin Lin <atimis19@gmail.com>
2017-08-08 14:48:21 +0800

    Timer func done

---
+++ MelodySampling/MelodySampling/ScoresFunc.swift
@@ -0,0 +1,23 @@
+//
+//  ScoresFunc.swift
+//  MelodySampling
+//
+//  Created by moon on 2017/8/8.
+//  Copyright © 2017年 Marvin Lin. All rights reserved.
+//
+
+import Foundation
+
+func scoreAfterOneSong(time usedTime: Double) -> Int {
+    
+    let score: Double?
+    
+    if usedTime > 30.0 {
+        score = 600.0
+    } else {
+        score = 3600.0 - 100.0 * usedTime
+    }
+    
+    return Int(score!)
+    
+}
