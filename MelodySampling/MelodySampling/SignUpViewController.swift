082c01d530631829bcda7a966ca8763542c1e872
Marvin Lin <atimis19@gmail.com>
2017-08-10 10:23:42 +0800

    UIAlert func done

--- MelodySampling/MelodySampling/SignUpViewController.swift
+++ MelodySampling/MelodySampling/SignUpViewController.swift
@@ -7,17 +7,45 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
+
+    var ref: DatabaseReference!
 
     @IBOutlet weak var opacityView: UIView!
 
     @IBOutlet weak var signUpLabel: UILabel!
 
+    @IBOutlet weak var emailTextField: UITextField!
+
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+
     @IBOutlet weak var signUpButtonOutlet: UIButton!
+
+    @IBOutlet weak var goToLoginLabel: UILabel!
 
     @IBAction func signUpButtonTapped(_ sender: UIButton) {
         print("Sign up button tapped")
+
+        if emailTextField.text == "" || passwordTextField.text == "" || confirmPasswordTextField.text == "" {
+
+            let emptyInputAlert = UIAlertController(title: "Empty field found!", message: "Please input all text field", preferredStyle: .alert)
+
+            let okAction = UIAlertAction(title: "Got it", style: .default) {_ in
+            }
+
+            emptyInputAlert.addAction(okAction)
+
+            self.present(emptyInputAlert, animated: true, completion: nil)
+
+        } else {
+
+           print("start judge")
+
+        }
     }
 
     override func viewDidLoad() {
@@ -26,6 +54,8 @@ class SignUpViewController: UIViewController {
         createSignUpPageGradient(target: opacityView)
 
         setCornerRadiustTo(signUpLabel)
+
+        goToLoginLabel.backgroundColor = UIColor.clear
 
         signUpButtonOutlet.setTitleColor(UIColor.clear, for: .normal)
     }
