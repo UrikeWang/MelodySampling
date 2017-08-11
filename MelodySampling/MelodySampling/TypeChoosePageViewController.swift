2192b5c10bd73e22f3b7771b8c1377cb1d503cbb
Marvin Lin <atimis19@gmail.com>
2017-08-11 17:41:19 +0800

    Type choose protolayout done

--- MelodySampling/MelodySampling/TypeChoosePageViewController.swift
+++ MelodySampling/MelodySampling/TypeChoosePageViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class TypeChoosePageViewController: UIViewController {
+class TypeChoosePageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var typeList = ["男歌手", "女歌手", "團體歌手", "單人歌手"]
+
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,4 +27,24 @@ class TypeChoosePageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return typeList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cellIdentifier = "TypeCell"
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = typeList[indexPath.row]
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+
+        return self.tableView.frame.height / CGFloat(typeList.count)
+
+    }
 }
