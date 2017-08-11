169c1292e936091992421461678249fb58194d1d
Marvin Lin <atimis19@gmail.com>
2017-08-11 17:20:25 +0800

    New Viewcontroller file installed

---
+++ MelodySampling/MelodySampling/TypeChoosePageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TypeChoosePageViewController.swift
+//  MelodySampling
+//
+//  Created by moon on 2017/8/11.
+//  Copyright © 2017年 Marvin Lin. All rights reserved.
+//
+
+import UIKit
+
+class TypeChoosePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
