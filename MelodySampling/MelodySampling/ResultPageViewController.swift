8de0c5c1f9c8a8b8e9cb7ee1f19f965c75c1991c
Marvin Lin <atimis19@gmail.com>
2017-08-11 14:46:56 +0800

    Result Cell height setting done

--- MelodySampling/MelodySampling/ResultPageViewController.swift
+++ MelodySampling/MelodySampling/ResultPageViewController.swift
@@ -12,6 +12,10 @@ class ResultPageViewController: UIViewController, UITableViewDelegate, UITableVi
 
     @IBOutlet weak var tableView: UITableView!
 
+    @IBOutlet weak var profilePageView: UIView!
+
+    @IBOutlet weak var lowerView: UIView!
+
     var songList = ["a", "b", "c", "d", "e"]
 
     override func viewDidLoad() {
@@ -32,9 +36,23 @@ class ResultPageViewController: UIViewController, UITableViewDelegate, UITableVi
 
         let cellIdentifier = "ResultCell"
 
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        //swiftlint:disable force_cast
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! ResultTableViewCell
+        //swiftlint:enable
+
+        cell.textLabel?.text = songList[indexPath.row]
 
         return cell
     }
 
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+
+        let screenSize = UIScreen.main.bounds
+
+        let screenHeight = screenSize.height
+
+        let tableHeight = screenHeight - profilePageView.frame.height - lowerView.frame.height
+
+        return tableHeight / CGFloat(songList.count)
+    }
 }
