6556671b104748b32a820957d10dbe1641c4404a
Marvin Lin <atimis19@gmail.com>
2017-08-19 09:31:46 +0800

    Delegate manager failed, B4 revising it

--- MelodySampling/MelodySampling/DistractorManager.swift
+++ MelodySampling/MelodySampling/DistractorManager.swift
@@ -12,6 +12,9 @@ import Firebase
 import CoreData
 import SwiftyJSON
 
+//失敗了，重新想一個方式
+//如果我只要15個，那就直接使用 random，範圍就在 firebase 上的題庫最大值，然後塞入 seedArray, while seedArray.count <15, 就不斷的取值，每一次取值都要比對 seedArray 裡面的值，沒出現過的才塞入。
+
 protocol DistractorManagerDelegate: class {
     
     func manager(_ manager: DistractorManager, didGet distractors: [String])
@@ -31,7 +34,7 @@ class DistractorManager {
         case invalidResponse
     }
 
-    func getOneDistractor(input random: Int, completion: @escaping (_ distracor: String) -> Void) {
+    func getOneDistractor(input random: Int, completion: @escaping ( _ distracor: String) -> Void) {
 
         ref = Database.database().reference()
 
@@ -63,8 +66,8 @@ class DistractorManager {
             
             let input = random(400)
             
-            getOneDistractor(input: input, completion: { result in
-                self.distractors.append(result)
+            getOneDistractor(input: input, completion: { download in
+                self.distractors.append(download)
                 
                 if self.distractors.count == 15 {
                     
