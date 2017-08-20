adc08dfe46e43dc101a25ab9e650cc24ef57f157
Marvin Lin <atimis19@gmail.com>
2017-08-20 17:19:08 +0800

    Result Page don't save albumPic

--- MelodySampling/MelodySampling/ResultPageViewController.swift
+++ MelodySampling/MelodySampling/ResultPageViewController.swift
@@ -33,12 +33,14 @@ class ResultPageViewController: UIViewController, UITableViewDelegate, UITableVi
 
     @IBOutlet weak var invisibleNextGameButtonOutlet: UIButton!
     @IBAction func invisibleNextGameButtonTapped(_ sender: UIButton) {
+        saveResultToHistory()
         gotoTypeChoosePage(from: self)
     }
 
     @IBOutlet weak var invisibleGoHomeButtonOutlet: UIButton!
 
     @IBAction func invisibleGoHomeButtonTapped(_ sender: UIButton) {
+        saveResultToHistory()
         gotoProfilePage(from: self)
     }
 
@@ -159,8 +161,8 @@ class ResultPageViewController: UIViewController, UITableViewDelegate, UITableVi
             resultsArray.append(temp)
 
         }
-
-        saveResultToHistory()
+//MARK: 把這個指令放到按下 button
+//        saveResultToHistory()
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -226,6 +228,7 @@ class ResultPageViewController: UIViewController, UITableViewDelegate, UITableVi
 
             var image = UIImage(named: "collectionPlaceHolder")
 
+            /*
             let fileURL = URL(fileURLWithPath: self.documentsURL.appending("artworkImage\(picIndex).jpg"))
 
             do {
@@ -237,6 +240,7 @@ class ResultPageViewController: UIViewController, UITableViewDelegate, UITableVi
             } catch {
                 print("image didn't download yet")
             }
+ */
 
             if let artistID = question.artistID, let artistName = question.artistName, let trackID = question.trackID, let trackName = question.trackName, let artworkUrl = question.artworkUrl, let previewUrl = question.previewUrl, let collectionID = question.collectionID, let collectionName = question.collectionName, let primaryGenreName = question.primaryGenreName {
 
@@ -244,7 +248,7 @@ class ResultPageViewController: UIViewController, UITableViewDelegate, UITableVi
 
                     self.historyMO = HistoryMO(context: appDelegate.persistentContainer.viewContext)
 
-                    self.historyMO.artworkImage = imageData
+                    self.historyMO.artworkImage = UIImagePNGRepresentation(UIImage(named: "collectionPlaceHolder")!) as! NSData
 
                     picIndex += 1
 
