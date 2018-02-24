4b2fd857d2788648f7d3e7d79fd1665b4cb27d3e
Marvin Lin <atimis19@gmail.com>
2018-02-24 23:44:23 +0800

    Date Utility init.

--- MelodySampling/MelodySampling/PlayingNavigationController.swift
+++ MelodySampling/MelodySampling/PlayingNavigationController.swift
@@ -12,16 +12,12 @@ import Firebase
 class PlayingNavigationController: UINavigationController {
 
     var questionArray = [EachQuestion]()
-    
     var resultArray = [ResultToShow]()
-    
     var distractorArray = [String]()
-    
     var randomUser: RandomUser.Results?
-    
     var randomUserImage = UIImage()
-
     var ref: DatabaseReference!
+    var unixTimestamp: Double?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,19 +27,28 @@ class PlayingNavigationController: UINavigationController {
         let timeDict = ["timestamp": ServerValue.timestamp()] as [String: Any]
         timePath.updateChildValues(timeDict)
     }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
         self.ref = Database.database().reference()
         let timePath = ref.child("currentTime")
-        timePath.observeSingleEvent(of: .value, with: { (snapshot) in
-            // FIXME: 聽到的日期是放在之後的比賽裡面
+        
+        timePath.observe( .value, with: { (snapshot) in
             guard let value = snapshot.value as? [String: Any],
-                let timestamp = value["timestamp"] else { return }
+                let timestamp = value["timestamp"] as? Double else { return }
+            
+            self.unixTimestamp = timestamp / 1000
+            let date = Date(timeIntervalSince1970: self.unixTimestamp!)
+            let strDate = DateUtility.getStrDate(from: date)
+            let weekOfYear = DateUtility.getWeekOfYear(from: date)
+            
             print("目前聽到的時間: \(timestamp)")
-
+            print("轉換後的時間: \(strDate)")
+            print("週數: \(weekOfYear)")
         }) { (err) in
             print("error :\(err)")
         }
+        
     }
 }
